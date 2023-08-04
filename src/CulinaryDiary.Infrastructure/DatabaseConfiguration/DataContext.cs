c854c655e777ca0cc1d496452d5515edc2518c69
Maciej Gródecki <maciej.grodecki@outlook.com>
2023-08-04 13:54:17 +0200

    Add DataContext configuration for identity

---
+++ src/CulinaryDiary.Infrastructure/DatabaseConfiguration/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using CulinaryDiary.Core.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CulinaryDiary.Infrastructure.DatabaseConfiguration
+{
+    public class DataContext : IdentityDbContext<AppUser, AppRole, Guid, IdentityUserClaim<Guid>, AppUserRole,
+        IdentityUserLogin<Guid>, IdentityRoleClaim<Guid>, IdentityUserToken<Guid>>
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<AppUser>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.AppUser)
+                .HasForeignKey(ur => ur.UserId)
+                .IsRequired();
+
+            builder.Entity<AppRole>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.AppRole)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired();
+        }
+    }
+}
