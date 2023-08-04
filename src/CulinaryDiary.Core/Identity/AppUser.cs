1d89497b6f15a5ce28659982265cefe2a9560a8f
Kernius89 <134935105+Kernius89@users.noreply.github.com>
2023-08-04 13:58:32 +0200

    Feature/003 create asp net core identity (#3)
    * Add Identity models
    
    * Add DataContext configuration for identity
    
    ---------
    
    Co-authored-by: Maciej Gródecki <maciej.grodecki@outlook.com>

---
+++ src/CulinaryDiary.Core/Identity/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CulinaryDiary.Core.Identity
+{
+    public class AppUser : IdentityUser<Guid>
+    {
+        public ICollection<AppUserRole>? UserRoles { get; set; }
+    }
+}
---
+++ src/CulinaryDiary.Core/Identity/AppUserRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CulinaryDiary.Core.Identity
+{
+    public class AppUserRole : IdentityUserRole<Guid>
+    {
+        public AppUser? AppUser { get; set; }
+        public AppRole? AppRole { get; set; }
+    }
+}
