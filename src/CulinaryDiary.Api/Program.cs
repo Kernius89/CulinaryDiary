a685bf559fe20d853b6bb7ed34d7ab4cfe4ede09
Kernius89 <134935105+Kernius89@users.noreply.github.com>
2023-08-04 13:20:37 +0200

    Create project structure (#1)
    * Add CulinaryDiary.Core project
    
    * Add CulinaryDiary.Infrastructure project
    
    * Add CulinaryDiary.Api project
    
    ---------
    
    Co-authored-by: Maciej Gródecki <maciej.grodecki@outlook.com>

---
+++ src/CulinaryDiary.Api/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
