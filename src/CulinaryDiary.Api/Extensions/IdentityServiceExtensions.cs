befb32b9c4bb422d1bbcc4470829f00bb670e144
Maciej Gródecki <maciej.grodecki@outlook.com>
2023-08-09 12:24:50 +0200

    Add IdentityServiceExtensions

---
+++ src/CulinaryDiary.Api/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,23 @@
+﻿using CulinaryDiary.Core.Identity;
+using CulinaryDiary.Infrastructure.DatabaseConfiguration;
+using Microsoft.AspNetCore.Identity;
+
+namespace CulinaryDiary.Api.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityService(this IServiceCollection services)
+        {
+            services.AddIdentityCore<AppUser>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 8;
+            })
+                .AddRoles<AppRole>()
+                .AddRoleManager<RoleManager<AppRole>>()
+                .AddEntityFrameworkStores<DataContext>();
+
+            return services;
+        }
+    }
+}
